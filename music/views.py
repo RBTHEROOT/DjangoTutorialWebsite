2ec455c84664f15b7c6b2089ab5cbd7b4870d0e9
Raja <hathtech.com@gmail.com>
2016-08-20 19:06:09 +0530

    Render function

--- music/views.py
+++ music/views.py
@@ -6,12 +6,11 @@ from .models import Album
 def index(request):
     html = ' '
     all_album = Album.objects.all()
-    template = loader.get_template('music/index.html')
     context = {
         'all_album': all_album,
     }
 
-    return HttpResponse(template.render(context, request))
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     return HttpResponse("<h2> Details of Album Number: "+ album_id + "</h2>")
