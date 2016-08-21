1f3eddb7517215ba396a59ebc8a2112741121927
Raja <hathtech.com@gmail.com>
2016-08-21 07:59:48 +0530

    Different set of url for different app. get_object_or_404() is added in place of try and except

--- music/views.py
+++ music/views.py
@@ -1,14 +1,11 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
     all_album = Album.objects.all()
     return render(request, 'music/index.html', {'all_album': all_album, })
 
-def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album Does not exists")
+def detail(request,album_id):
+    #album = Album.objects.get(pk=album_id)
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/details.html', {'album': album})
