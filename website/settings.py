eda0db3a39719a4db1c9d497ffda3ed695dc0f39
Raja <hathtech.com@gmail.com>
2016-08-21 18:18:17 +0530

    Style link and files are added

--- website/settings.py
+++ website/settings.py
@@ -57,7 +57,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            'template',
+            'music/template',
         ],
         'APP_DIRS': True,
         'OPTIONS': {
