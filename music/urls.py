77019593d8b7ca4a6f60de31130371c028f8cff5
Raja <hathtech.com@gmail.com>
2016-08-21 09:34:51 +0530

    Added favorite function in views. Edited details view to have favorite level and submit button.

--- music/urls.py
+++ music/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     # /music/
     url(r'^$', views.index, name='index'),
+    # /music/712/favorite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
     # /music/712/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
