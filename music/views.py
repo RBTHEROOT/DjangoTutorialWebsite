b8b1a963f63563cf2a9a474fda50d2a177ba5658
Raja <hathtech.com@gmail.com>
2016-08-24 20:31:16 +0530

    Added update and delete functionality.

--- music/views.py
+++ music/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from .models import Album
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
@@ -16,6 +17,17 @@ class DetailsView(generic.DetailView):
     model = Album
     template_name = 'music/details.html'
 
+
+
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
